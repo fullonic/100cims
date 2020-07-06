61ab0ccf6e3823d3972766335b9049e90a89d552
fullonic <somnium@riseup.net>
2020-07-06 17:38:35 +0200

    Add schemas for Cim and Route

--- schemas.py
+++ schemas.py
@@ -1,14 +1,37 @@
 """Application schemas."""
 from pydantic import BaseModel
+from typing import List
+
+
+class Route(BaseModel):
+    """Route obj schema."""
+
+    id: int
+    name: str
+    uuid: str
+    cim_uuid: str
+    length: float
+    type: str
+    circular: bool
+    ramp_up: float
+    ramp_down: float
+    max_hight: float
+    min_hight: float
+    difficulty: str
+    url: str
 
 
 class Cim(BaseModel):
-    """Cim obj class."""
+    """Cim obj schema."""
 
+    id: int
+    uuid: str
     name: str
     region: str
     lat: float
     lng: float
     alt: int
+    essential: bool
     url: str
     img_url: str
+    routes: List[Route]
